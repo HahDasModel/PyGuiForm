44f9dd7c31d1abbd2e6bd1abdd586b85236bc572
HahDasModel <59003316+HahDasModel@users.noreply.github.com>
2021-06-29 19:49:51 +1000

    Add files via upload

--- Open_Chr.py
+++ Open_Chr.py
@@ -1,29 +1,14 @@
-# -*- encoding: utf-8 -*-
-#from __future__ import print_function
 import time
 import pyautogui as gui
 import os
-#import sys
-#while True:
-#	gui.click(700, 500)
+
 # print(gui.position())
 
 os.startfile(r"C:\Users\Khodu\Desktop\PyPr\Chr_Full_Scr.bat")
 time.sleep(2)
 gui.click(146, 53)
-#gui.doubleClick(146, 53)
-gui.hotkey('backspace')
+
 time.sleep(2)
 gui.typewrite(u'Да', 0.2)
 gui.typewrite(u'Да', 0.2)
 print('Ммммм')
-#i=0
-#for i in range(0, len(g)):
-#	print(g[i])
-#	g=+1
-
-	
-
-# Вводим в поисковик 146, 53
-# пишем как же все задолбало
-# break
